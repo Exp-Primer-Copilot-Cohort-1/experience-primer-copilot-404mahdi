4b528e37a0a789991331d5752723c174aa1cdbcc
Mahdi Hasan <119351159+404mahdi@users.noreply.github.com>
2023-09-23 17:08:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,60 +1,18 @@
-// create web server
+// Create web server
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
+// 1. Import Express
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// create web server
-http.createServer(function (req, res) {
-    var pathname = url.parse(req.url).pathname;
-    var extname = path.extname(pathname);
-    var mimeType = getMimeType(extname);
-    var filePath = path.join(__dirname, pathname);
+// 2. Import mongoose
+const mongoose = require('mongoose');
 
-    if (extname === '.js' || extname === '.css') {
-        fs.readFile(filePath, function (err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Could not find or open file for reading\n');
-            } else {
-                res.writeHead(200, { 'Content-Type': mimeType });
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World\n');
-    }
-}).listen(3000);
+// 3. Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/express-demo', { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
-console.log('Server running at http://localhost:3000/');
-
-// get mime type
-function getMimeType(extname) {
-    var mimeType = null;
-    switch (extname) {
-        case '.html':
-            mimeType = 'text/html';
-            break;
-        case '.js':
-            mimeType = 'text/javascript';
-            break;
-        case '.css':
-            mimeType = 'text/css';
-            break;
-        case '.jpg':
-            mimeType = 'image/jpeg';
-            break;
-        case '.png':
-            mimeType = 'image/png';
-            break;
-        case '.gif':
-            mimeType = 'image/gif';
-            break;
-        case '.ico':
-            mimeType = 'image/icon';
-            break;
-    }
-    return mimeType;
-}
+// 4. Create a schema
+const courseSchema = new mongoose.Schema({
+    name: String,
